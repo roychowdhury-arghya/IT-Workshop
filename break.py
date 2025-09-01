80e84760159bb008555bfd0e93f1cba5c8762981
Argha Roy Chowdhury <arghyarancho@gmail.com>
2025-09-01 20:23:39 +0530

    divisible and break

---
+++ break.py
@@ -0,0 +1,6 @@
+while True:
+    user_input = input("Enter Something(type'exit' to stop) : ")
+    if user_input.lower() =="exit":
+        print("Exting the program...")
+        break
+    print("You entered",user_input)
---
+++ divisible-3-5.py
@@ -0,0 +1,5 @@
+count = 0
+for i in range (1,101):
+    if i%3==0 and i%5 == 0:
+        count+=1
+print("Count of numbers divisible by 3 and 5 :",count)
