082b615fc9adbeb91e233c39daf2d8b356e478a9
Argha Roy Chowdhury <arghyarancho@gmail.com>
2026-02-09 20:36:56 +0530

    LAb one

---
+++ lab1.py
@@ -0,0 +1,52 @@
+a = 10
+b = 3.14
+c = "Arghya"
+d = True
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+x = 5
+print(x, type(x))
+
+x = "Dipon"
+print(x, type(x))
+
+b = "Sheetal"
+print(b, type(b))
+
+a, b, c = 10, 20, 30
+print(a, b, c)
+x = y = z = 100
+print(z)
+
+a = 10
+b = a
+a = 20
+print(b)
+
+a = 10
+b = 3
+
+print(a + b)   # addition
+print(a - b)   # subtraction
+print(a * b)   # multiplication
+print(a / b)   # division
+print(a % b)   # remainder
+
+#logical / comparison operator
+a = 10
+b = 20
+
+print(a > b)
+print(a == b)
+
+x = 10
+print(x > 5 and x < 20)
+print('a' in "apple") #membership operator
+
+def dj():
+    print("Siuu!")
+
+dj()
