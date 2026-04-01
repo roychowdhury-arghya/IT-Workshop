afcd79a0c43eb0328a0f7ad7b422ba59918866a2
Argha Roy Chowdhury <arghyarancho@gmail.com>
2026-04-02 02:11:53 +0530

    practice

---
+++ practice.py
@@ -0,0 +1,41 @@
+for i in range (5):
+    print (i)
+name = "Arghya"
+roll = 1
+cgpa = 9.23
+student = True
+
+print(student,type(student))
+print(name,type(name))
+print(roll,type(roll))
+print(cgpa,type(cgpa))
+
+a,b,c=1,2,3
+print(a,b,c)
+x=y=z=11
+print(x,y,z)
+
+
+# x = 10
+# y=5
+# print(x > 5 and y < 10) # True
+# print(x > 20 or y < 10) # True
+# print(not(x > 5))
+# # False
+
+fruit = "apple"
+print('g' in fruit)
+print('g' not in fruit)
+
+msg = "welcome to india"
+print(msg)
+print(msg.capitalize())
+print(msg.upper())
+print(msg.count("i"))
+print(msg.find("to"))
+print(msg.replace("india","kolkata"))
+print(msg)
+print(msg[-7:-1])
+print(msg[-1])
+print(msg[7:])
+print(msg[0:10])
