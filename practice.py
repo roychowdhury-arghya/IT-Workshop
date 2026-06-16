bad42f73b3351986aa1d76e6e1fd5498cfff1543
Argha Roy Chowdhury <arghyarancho@gmail.com>
2026-06-16 16:17:49 +0530

    edited

--- practice.py
+++ practice.py
@@ -39,3 +39,45 @@ print(msg[-7:-1])
 print(msg[-1])
 print(msg[7:])
 print(msg[0:10])
+
+
+# list in python 
+shopping_list=["milk","potato","apple","banana"]
+cooking_list=["curry","fry"]
+city = ["kolkata","mumbai","delhi"]
+print(shopping_list)
+print("apple" in shopping_list)
+print("mango" in shopping_list)
+print("First item :", shopping_list[0])
+print("Last element :",shopping_list[-1])
+print(shopping_list+cooking_list)
+print(len(shopping_list))
+print(city)
+city[2]="pune"
+print(city)
+city.append("surat")
+print(city)
+item = city.pop()
+print(city)
+print("popped item : ", item )
+city.remove("mumbai")
+print(city)
+
+
+# Fibonacci Number
+n = int(input("Enter number of terms: "))
+a, b =1, 2
+
+for i in range(n):
+    print(a, end="\n")
+    a, b = b, a + b
+
+person = ("Alice", 28, "Kolkata")
+print(person[0])
+print(person[-1])
+
+t1 = (1, 2, 3)
+t2 = (4, 5)
+print(t1 + t2)
+print(t2 * 2)
+print(t1[1:3])
